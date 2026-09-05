0a6d82ff9c6155bca5d97163b258271a5f0f2d6b
Adeqq1 <aderifqyreg.ti2@gmail.com>
2026-09-05 07:09:15 +0700

    variable

--- belajar-golang-dasar/string.go
+++ belajar-golang-dasar/string.go
@@ -6,4 +6,8 @@ func main(){
 	fmt.Println("Ade")
 	fmt.Println("Ade Aulian")
 	fmt.Println("Ade Rifqy Aulian")
+
+	fmt.Println(len("Ade"))
+	fmt.Println("Ade Aulian"[0])
+	fmt.Println("Ade Rifqy Aulian"[1])
 }
---
+++ belajar-golang-dasar/variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string
+
+	name = "Ade Aulian"
+	fmt.Println(name)
+
+	name = "Ade Rifqy"
+	fmt.Println(name)
+
+	name2 := "Ade Rifqy"
+	fmt.Println(name2)
+
+	name1 := "Ade Aulian"
+	fmt.Println(name1)
+
+	var (
+		firstName = "Ade"
+		lastName = "Aulian"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
