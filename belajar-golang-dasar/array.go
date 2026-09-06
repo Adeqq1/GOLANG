9607a32a53632a928774db18b262458036a99dcb
Adeqq1 <aderifqyreg.ti2@gmail.com>
2026-09-06 22:58:46 +0700

    array

---
+++ belajar-golang-dasar/array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var names [3]string
+	names[0] = "Ade"
+	names[1] = "Rifqy"
+	names[2] = "Aulian"
+
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+	fmt.Println(names[0])
+}
