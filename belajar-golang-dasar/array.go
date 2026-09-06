189d13d3499abd7260ae63b322d42a22838e3e82
Adeqq1 <aderifqyreg.ti2@gmail.com>
2026-09-06 23:06:09 +0700

    array(1)

--- belajar-golang-dasar/array.go
+++ belajar-golang-dasar/array.go
@@ -11,4 +11,16 @@ func main(){
 	fmt.Println(names[1])
 	fmt.Println(names[2])
 	fmt.Println(names[0])
+
+	var values = [...]int{
+		90,
+		80,
+		95,
+		100,
+		110,
+	}
+	fmt.Println(values)
+	fmt.Println(len(values))
+	values[0] = 100
+	fmt.Println(values)
 }
