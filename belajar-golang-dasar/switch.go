3e979ba161247c1c8b91ed594ab1474e8514a5af
Adeqq1 <aderifqyreg.ti2@gmail.com>
2026-09-08 13:11:58 +0700

    switch expresion

---
+++ belajar-golang-dasar/switch.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Ade"
+
+	switch name {
+	case "Ade":
+		fmt.Println("Hello Ade")
+	case "Budi":
+		fmt.Println("Hello Budi")
+	case "Bro":
+		fmt.Println("Hello Bro")
+	default:
+		fmt.Println("Hello boleh kenalan?")
+	}
+
+	switch length := len(name); length > 5 {
+	case true:
+		fmt.Println("Nama terlalu panjang")
+	case false: 
+		fmt.Println("Nama sudah benar")
+	}
+
+	name = "Ade Ganteng"
+	length := len(name) 
+	switch {
+	case length > 10:
+		fmt.Println("Nama terlalu panjang")
+	case length > 5:
+		fmt.Println("Nama lumayan panjang")
+	default:
+		fmt.Println("Nama sudah benar")
+	}
+}
